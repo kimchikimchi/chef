89a22dbc8ec2c1d421971beef210e365caefc723
Jay Cha <jay.cha@experian.com>
2017-04-10 16:43:08 -0400

    - Using remote file fetching

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -2,14 +2,18 @@ package 'httpd' do
   action :install
 end
 
-cookbook_file '/var/www/html/index.html' do
-  source 'index.html'
+#cookbook_file '/var/www/html/index.html' do
+#  source 'index.html'
+#end
+
+remote_file '/var/www/html/google_logo.png' do
+    source 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
 end
 
-#template '/var/www/html/index.html' do
-#  source 'index.html.erb'
-#  action :create
-#end
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
+end
 
 service 'httpd' do
   action [:enable, :start]
